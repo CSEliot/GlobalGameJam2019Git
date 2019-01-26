27541e96730d210024800d69c8eeb336f9824624
TSCHIRGIMACHINE\pault_000 <pault1@umbc.edu>
2019-01-26 10:50:33 -0500

    Bunch of assets work and script work on player object interactions and home setup

--- Assets/Paul/Scripts/Collectable.cs
+++ Assets/Paul/Scripts/Collectable.cs
@@ -8,5 +8,6 @@ public class Collectable : MonoBehaviour
 {
     public List<PersonalityType> traits = new List<PersonalityType>();
 
-    
+    public Vector3 localPos;
+    public Vector3 localErot;
 }
