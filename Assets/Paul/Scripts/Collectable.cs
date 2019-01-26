ad2869dcc77dafc96130ba8c2dd315b7e4ad4dc1
Tad Klopcic <klopcicjt@gmail.com>
2019-01-26 13:46:07 -0500

    Added roomAffinity

--- Assets/Paul/Scripts/Collectable.cs
+++ Assets/Paul/Scripts/Collectable.cs
@@ -11,6 +11,7 @@ public enum CurrentPlace
 public class Collectable : MonoBehaviour
 {
     public List<PersonalityType> traits = new List<PersonalityType>();
+    public RoomType roomAffinity = RoomType.Store;
 
     public Vector3 localPos; //for parenting to character 
     public Vector3 localErot;
