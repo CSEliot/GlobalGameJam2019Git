eed87534cac7f109dcfe826600f1cd1c70faa10b
TSCHIRGIMACHINE\pault_000 <pault1@umbc.edu>
2019-01-27 09:40:55 -0500

    metas and placement script

--- Assets/Paul/Scripts/SetPlayerAndOthers.cs
+++ Assets/Paul/Scripts/SetPlayerAndOthers.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class SetPlayerAndOthers : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public PaulMovementPlaceholder mainPlayer;
+
+    public List<Transform> characterObjects = new List<Transform>();
+
+    public List<Transform> places = new List<Transform>();
+
     void Start()
     {
-        
+        //Invoke("SetEveryonesStart", 2f);
     }
 
     public void SetEveryonesStart()
     {
+        int mainPlayerSet = (int)mainPlayer.myPersonality;
 
+        characterObjects[0].position = places[mainPlayerSet].position;
+        characterObjects[0].rotation = places[mainPlayerSet].rotation;
+
+        switch (mainPlayerSet)
+        {
+            case 0:
+                {
+                    
+                    break;
+                }
+        }
     }
 
 }
