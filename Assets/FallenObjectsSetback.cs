76a80c228027ab946c7b7b275cff25e8540d53e6
TSCHIRGIMACHINE\pault_000 <pault1@umbc.edu>
2019-01-27 16:22:03 -0500

    some changes probably

---
+++ Assets/FallenObjectsSetback.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallenObjectsSetback : MonoBehaviour
+{
+    
+    void OnTriggerEnter(Collider col)
+    {
+        //col.GetComponent<Collectable>();
+    }
+}
